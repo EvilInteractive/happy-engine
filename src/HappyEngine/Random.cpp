052cf99bd860028f69ddeaf039a5509ec34e18a7
bastian.damman <bastian.damman@5aa1a52c-6ec2-ce28-c747-3d7cbe9a755d>
2011-09-01 16:35:32 +0000

    Pseudo random class: seed x always gives the same random
    git-svn-id: https://happy-engine.googlecode.com/svn/trunk@41 5aa1a52c-6ec2-ce28-c747-3d7cbe9a755d

---
+++ src/HappyEngine/Random.cpp
@@ -0,0 +1,55 @@
+//HappyEngine Copyright (C) 2011  Bastian Damman, Sebastiaan Sprengers
+//
+//This file is part of HappyEngine.
+//
+//    HappyEngine is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    HappyEngine is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with HappyEngine.  If not, see <http://www.gnu.org/licenses/>.
+//
+//Author:  Bastian Damman
+//Created: 01/09/2011
+
+#include "Random.h"
+#include "HappyNew.h"
+
+namespace happyengine {
+
+Random::Random(uint seed): 
+    m_RandomEngine(seed),
+    m_Generator(m_RandomEngine, boost::uniform_real<double>(0.0, 1.0)),
+    m_Seed(seed)
+{
+}
+
+Random::~Random()
+{
+}
+
+int Random::nextInt(int min, int max)
+{
+    return static_cast<int>(min + m_Generator() * (max - min));
+}
+float Random::nextFloat(float min, float max)
+{
+    return static_cast<float>(min + m_Generator() * (max - min));
+}
+double Random::nextDouble(double begin, double max)
+{
+    return begin + m_Generator() * (max - begin);
+}
+
+uint Random::getSeed() const
+{
+    return m_Seed;
+}
+
+} //end namespace
---
+++ src/HappyEngine/Random.h
@@ -0,0 +1,58 @@
+//HappyEngine Copyright (C) 2011  Bastian Damman, Sebastiaan Sprengers
+//
+//This file is part of HappyEngine.
+//
+//    HappyEngine is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    HappyEngine is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with HappyEngine.  If not, see <http://www.gnu.org/licenses/>.
+//
+//Author:  Bastian Damman
+//Created: 01/09/2011
+
+#ifndef _RANDOM_H_
+#define _RANDOM_H_
+#pragma once
+
+#include "HappyTypes.h"
+#include "boost/random.hpp"
+#include <time.h>
+
+namespace happyengine {
+
+class Random
+{
+public:
+	explicit Random(uint seed = time(nullptr));
+	//Random();
+    virtual ~Random();
+
+    int nextInt(int min, int max);
+    float nextFloat(float min, float max);
+    double nextDouble(double min, double max);
+
+    uint getSeed() const;
+
+private:
+
+    uint m_Seed;
+
+    boost::mt19937 m_RandomEngine;
+    boost::variate_generator<boost::mt19937&, boost::uniform_real<double>> m_Generator;
+
+    //Disable default copy constructor and default assignment operator
+    Random(const Random&);
+    Random& operator=(const Random&);
+};
+
+} //end namespace
+
+#endif
