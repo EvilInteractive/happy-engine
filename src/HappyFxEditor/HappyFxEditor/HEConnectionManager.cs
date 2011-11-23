f12ae230544c16381e81e83ba981e4be897ebdca
bastian.damman <bastian.damman@5aa1a52c-6ec2-ce28-c747-3d7cbe9a755d>
2011-11-23 10:51:14 +0000

    first checkin HappyFxEditor
    git-svn-id: https://happy-engine.googlecode.com/svn/trunk@138 5aa1a52c-6ec2-ce28-c747-3d7cbe9a755d

---
+++ src/HappyFxEditor/HappyFxEditor/HEConnectionManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace HappyFxEditor
+{
+    class HEConnectionManager
+    {
+        TcpClient _client;
+
+        public bool Connected { get; private set; }
+        public string ConnectionInfo
+        {
+            get 
+            {
+                if (Connected)
+                    return "Connected!";
+                else
+                    return "Not Connected!";
+            }
+        }
+
+        public HEConnectionManager()
+        {
+            Connected = false;
+            _client.BeginConnect("localhost", 3250, ConnectedCallback, null);
+        }
+
+        void ConnectedCallback(object connectionInfo)
+        {
+            Connected = true;
+        }
+    }
+}
