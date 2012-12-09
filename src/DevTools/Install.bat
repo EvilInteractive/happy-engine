e6513582f3bae9386f9981425f6a01e34e6a7f91
Bastian Damman <bastian.damman@gmail.com>
2012-12-09 17:40:19 +0100

    typo

--- src/DevTools/Install.bat
+++ src/DevTools/Install.bat
@@ -3,7 +3,7 @@ DebugVisual.py
 @echo ""
 @echo ******************************************
 @echo If it failed, try running as administrator, 
-@echo or install python 1.7.*
+@echo or install python 2.7.*
 @echo ******************************************
 @echo ""
 pause
