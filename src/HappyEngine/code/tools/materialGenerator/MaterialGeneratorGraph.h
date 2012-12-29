1b2ac51daffb807b8fc8dc1b8a55062309086c10
sebastiaan.sprengers <sebastiaan.sprengers@gmail.com>
2012-12-29 20:59:18 +0100

    older freetype lib

--- src/HappyEngine/code/tools/materialGenerator/MaterialGeneratorGraph.h
+++ src/HappyEngine/code/tools/materialGenerator/MaterialGeneratorGraph.h
@@ -164,7 +164,7 @@ private:
         vec2 m_Position;
     };
     void increaseErrorPool(const size_t extraSize);
-    gfx::Font* m_ErrorFont;
+    gui::Font* m_ErrorFont;
     he::PrimitiveList<gui::Text*> m_ErrorPool;
     he::ObjectList<ErrorMessage> m_VisibleErrors;
     gui::Sprite* m_ErrorBackgroundSprite;
