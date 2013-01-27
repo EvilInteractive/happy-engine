1bfb9cac9848f4f3836cfc93c58d0572f4e7c420
ThijsM <thijs.morlion@gmail.com>
2013-01-27 17:41:57 +0100

    Thijs Test Level

---
+++ src/HappyTests/code/HappyThijsTest/CMakeLists.cmake
@@ -0,0 +1,20 @@
+set (HAPPYTHIJSTEST_CODE_FILES_SRC_FILTER    
+        main.cpp
+        MainGame.cpp            MainGame.h)
+
+set (HAPPYTHIJSTEST_CODE_FILES_PCH_FILTER    
+        HappyThijsTestPCH.cpp     HappyThijsTestPCH.h)
+                        
+foreach(f ${HAPPYTHIJSTEST_CODE_FILES_SRC_FILTER}) 
+    LIST(APPEND HAPPYTHIJSTEST_CODE_FILES_SRC_FILTER_SRCS ../../code/HappyThijsTest/${f})
+endforeach(f)           
+
+foreach(f ${HAPPYTHIJSTEST_CODE_FILES_PCH_FILTER}) 
+    LIST(APPEND HAPPYTHIJSTEST_CODE_FILES_PCH_FILTER_SRCS ../../code/HappyThijsTest/${f})
+endforeach(f) 
+
+source_group(src FILES ${HAPPYTHIJSTEST_CODE_FILES_SRC_FILTER_SRCS})
+source_group(pch FILES ${HAPPYTHIJSTEST_CODE_FILES_PCH_FILTER_SRCS})
+
+set (HAPPYTHIJSTEST_CODE_SRCS  ${HAPPYTHIJSTEST_CODE_FILES_SRC_FILTER_SRCS} 
+                   ${HAPPYTHIJSTEST_CODE_FILES_PCH_FILTER_SRCS})
