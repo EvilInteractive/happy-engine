c5e91ecc4076f76113c884e878a866e77ce230dd
Bastian Damman <bastian.damman@gmail.com>
2013-03-15 22:58:45 +0100

    Added HappyJson project (clone of jsoncpp (public domain))

---
+++ src/HappyJson/CMakeIncludeLists.cmake
@@ -0,0 +1,6 @@
+cmake_minimum_required (VERSION 2.8) 
+
+add_subdirectory (${HappyEngine_SOURCE_DIR}/HappyJson/build/HappyJson)
+add_subdirectory (${HappyEngine_SOURCE_DIR}/HappyJson/build/HappyJsonTest)
+add_subdirectory (${HappyEngine_SOURCE_DIR}/HappyJson/build/HappyJsonTestRunner)
+
