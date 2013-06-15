3694de991a107e3a5b3280fbda73cfc4c8a6b3b4
Bastian Damman <bastian.damman@gmail.com>
2013-06-15 23:59:11 +0200

    Added support for VC11

--- src/3thParty/INSTALL_WIN.bat
+++ src/3thParty/INSTALL_WIN.bat
@@ -4,17 +4,20 @@ installers\win\Assimp.exe
 installers\win\Awesomium.exe
 IF /I %vc12% == y (
 installers\win\Boost-msvc11.exe
+installers\win\PhysX-msvc11.exe
+installers\win\RakNet-msvc11.exe
+installers\win\SFML2.0-msvc11.exe
 ) ELSE (
 installers\win\Boost.exe
+installers\win\PhysX.exe
+installers\win\RakNet.exe
+installers\win\SFML2.0.exe
 )
 installers\win\DevIL.exe
 installers\win\FreeType.exe
 installers\win\glew.exe
 installers\win\lbsndfile.exe
 installers\win\OpenAl.exe
-installers\win\PhysX.exe
-installers\win\RakNet.exe
-installers\win\SFML2.0.exe
 installers\win\Cairo.exe
 installers\win\fontconfig.exe
 installers\win\LibOVR.exe
